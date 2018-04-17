60c4108f4a7ea79381fb3f925929a00d8939c8ec
Sergio Forero <sforero@endava.com>
2018-04-17 10:24:54 -0500

    Proyectos base agregados

--- LSC/cntkTest/src/main/java/com/lsc/cntkTest/services/CNTKService.java
+++
@@ -1,8 +0,0 @@
-package com.lsc.cntkTest.services;
-
-import com.lsc.cntkTest.dtos.CNTKResponseDTO;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface CNTKService {
-    CNTKResponseDTO postCNTK(MultipartFile file);
-}
--- LSC/serverTest/src/main/java/com/lsc/serverTest/repositories/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lsc.serverTest.repositories;
-
-import com.lsc.serverTest.entities.TestEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TestRepository extends MongoRepository<TestEntity, String> {
-}
